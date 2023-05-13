109d5d73bc78099b8df7a086c5a9ce4ed021a04f
Tom-Bumhead <psytm8@exmail.nottingham.ac.uk>
2023-05-13 10:59:59 +0100

    removed test Window Procedure

--- LearningToWindowsAgain/Source.cpp
+++ LearningToWindowsAgain/Source.cpp
@@ -11,17 +11,3 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
 	return 0;
 }
-
-
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
-{
-	switch (uMsg)
-	{
-	case WM_DESTROY:
-		PostQuitMessage(0);
-		return 0;
-		break;
-	default:
-		return DefWindowProc(hwnd, uMsg, wParam, lParam);
-	}
-}
